5bcee2b3cc0736d6e61811cb8ee2a200283f8372
Nucky <2237466533@qq.com>
2025-10-21 16:35:45 +0800

    Prepare Clear Processors

---
+++ Source/TrafficSIm/Private/ClearSpawnPointProcessor.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClearSpawnPointProcessor.h"
+
+UClearSpawnPointProcessor::UClearSpawnPointProcessor()
+{
+}
+
+void UClearSpawnPointProcessor::Initialize(UObject& Owner)
+{
+}
+
+void UClearSpawnPointProcessor::ConfigureQueries()
+{
+}
+
+void UClearSpawnPointProcessor::Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context)
+{
+}
---
+++ Source/TrafficSIm/Public/ClearSpawnPointProcessor.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassProcessor.h"
+#include "ClearSpawnPointProcessor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAFFICSIM_API UClearSpawnPointProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+public:
+	UClearSpawnPointProcessor();
+	virtual void Initialize(UObject& Owner) override;
+	virtual void ConfigureQueries() override;
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+
+
+private:
+	FMassEntityQuery EntityQuery;
+	
+};
